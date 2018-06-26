30afb602904821e7f2622b38ea85eed16813f587
Palmatori <33674126+palmatori@users.noreply.github.com>
2018-06-25 23:58:26 -0400

    boton con class

--- UnitTestPanaderia/RecetasUitTest.cs
+++ UnitTestPanaderia/RecetasUitTest.cs
@@ -13,19 +13,19 @@ namespace UnitTestPanaderia
         IWebDriver driver = new ChromeDriver();
 
         [Test]
-        public void RegistroRecetaTest()
+        public void RegistroReceta()
         {
             driver.Navigate().GoToUrl(url + "/Home/Login");
 
             driver.FindElement(By.Id("nombre")).SendKeys("ariel");
             driver.FindElement(By.Id("clave")).SendKeys("1234");
-         
-            driver.FindElement(By.XPath("/html/body/form/div/div/div/div[2]/div[4]/div/input")).Click();
+
+            // driver.FindElement(By.XPath("/html/body/form/div/div/div/div[2]/div[4]/div/input")).Click();
+            driver.FindElement(By.ClassName("btn-block")).Click();
+
             driver.Navigate().GoToUrl(url + "/Recetas");
             driver.FindElement(By.Id("crear")).Click();
-            //IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/header/div/a/img"));
-            //IWebElement element = driver.FindElement(By.Id("Id"));
-            driver.FindElement(By.Id("Id")).SendKeys("210");
+            driver.FindElement(By.Id("Id")).SendKeys("211");
             driver.FindElement(By.Id("nombre")).SendKeys("chao");
             driver.FindElement(By.Id("valor_venta")).SendKeys("12");
             driver.FindElement(By.ClassName("btn")).Click();

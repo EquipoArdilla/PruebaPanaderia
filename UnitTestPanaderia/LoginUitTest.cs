119d040580fffa87b89a539cfbf1b7a5c4af0335
sebastianoyarzun <soportelaunion@gmail.com>
2018-06-25 23:46:48 -0300

    Master
    Master

---
+++ UnitTestPanaderia/LoginUitTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPanaderia
+{
+    [TestClass]
+    public class LoginUitTest
+    {
+        string url = "http://localhost:60656";
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void LoginTest()
+        {
+            driver.Navigate().GoToUrl(url + "/Home/Login");
+
+            driver.FindElement(By.Id("nombre")).SendKeys("ariel");
+            driver.FindElement(By.Id("clave")).SendKeys("1234");
+
+            driver.FindElement(By.XPath("/html/body/form/div/div/div/div[2]/div[4]/div/input")).Click();
+
+
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/header/div/a/img"));
+
+            String src_imagen = element.GetAttribute("src");
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(url + "/Img/ardilla-meme-panadero.jpg", src_imagen);
+
+
+
+            driver.Close();
+
+            driver.Quit();
+        }
+
+
+
+    }
+}
---
+++ UnitTestPanaderia/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPanaderia")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UnitTestPanaderia")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19abaaa6-2cab-4600-b9d2-e4218dae6a63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

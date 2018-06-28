52b273808b1dc5803e1501b22521160290b2c56d
Palmatori <33674126+palmatori@users.noreply.github.com>
2018-06-28 02:00:24 -0400

    se crean las 5 pruebas unitarias

--- UnitTestPanaderia/RecetasUitTest.cs
+++ UnitTestPanaderia/RecetasUitTest.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace UnitTestPanaderia
 {
@@ -15,6 +16,7 @@ namespace UnitTestPanaderia
         [Test]
         public void RegistroRecetaTest()
         {
+            int idBuscaNumero = 0;
             // Ingreso Login
             driver.Navigate().GoToUrl(url + "/Home/Login");
             driver.FindElement(By.Id("nombre")).SendKeys("ariel");
@@ -25,11 +27,36 @@ namespace UnitTestPanaderia
             driver.Navigate().GoToUrl(url + "/Recetas");
 
             //presiona click en botón crear
-            driver.FindElement(By.Id("crear")).Click(); 
+            driver.FindElement(By.Id("crear")).Click();
 
             //Genero id Aleatoreo
             Random rnd = new Random();
-            var idBusca =  Convert.ToString(rnd.Next(0, 100)); 
+
+            try
+            {
+                // verifico si exite en registro 1
+                IWebElement elementValido = driver.FindElement(By.Id(Convert.ToString(1)));
+                idBuscaNumero = 1;
+            }
+            catch (Exception)
+            {
+            
+                idBuscaNumero = rnd.Next(0, 100);
+            }
+
+            try
+            {
+                // verifico que id no este en db
+                IWebElement elementValido = driver.FindElement(By.Id(Convert.ToString(idBuscaNumero)));
+                idBuscaNumero = idBuscaNumero + rnd.Next(idBuscaNumero, 100);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No existe producto");
+            }
+
+            //convierto id para text
+            String idBusca =Convert.ToString(idBuscaNumero);
 
             //Ingreso registros
             driver.FindElement(By.Id("Id")).SendKeys(idBusca);
@@ -83,7 +110,7 @@ namespace UnitTestPanaderia
             driver.FindElement(By.Id("valor_venta")).SendKeys("14");
             driver.FindElement(By.Id("estado")).Click();
             
-            //Presiono boton
+            //click boton
             driver.FindElement(By.ClassName("btn")).Click();
 
             //Busco elemento id para optener atributos y verificar  que el id existe 
@@ -93,38 +120,199 @@ namespace UnitTestPanaderia
             //comparo elementos
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(nombreModifico, textoAtributo);
 
+            // Cierro Driver y ventana 
             driver.Close();
             driver.Quit();
         }
 
         [Test]
-        public void AgregaInsumoTest()
+        public void RecetaNoActivaTest()
         {
-            //string idBusca = "1";
-            //string buscaTexto = "insumo " + idBusca;
-            //string buscaNombre = "nombreReceta";
+            // Variables para busqueda 
+            string idBusca = "1";
 
-            //driver.Navigate().GoToUrl(url + "/Home/Login");
-            //driver.FindElement(By.Id("nombre")).SendKeys("ariel");
-            //driver.FindElement(By.Id("clave")).SendKeys("1234");
-            //driver.FindElement(By.ClassName("btn-block")).Click();
+            //Id para comprobar nombre de variables
+            string buscaNombre = "estado "+ idBusca;
 
-            //driver.Navigate().GoToUrl(url + "/Recetas");
+            String textoAtributo = "Busco";
 
-            //driver.FindElement(By.Id(idBusca)).Click();
-            //driver.FindElement(By.Id("nombre")).Clear();
-            //driver.FindElement(By.Id("valor_venta")).Clear();
-            //driver.FindElement(By.Id("nombre")).SendKeys(nombreModifico);
-            //driver.FindElement(By.Id("valor_venta")).SendKeys("14");
-            //driver.FindElement(By.Id("estado")).Click();
-            //driver.FindElement(By.ClassName("btn")).Click();
+            // Ingreso Login
+            driver.Navigate().GoToUrl(url + "/Home/Login");
+            driver.FindElement(By.Id("nombre")).SendKeys("ariel");
+            driver.FindElement(By.Id("clave")).SendKeys("1234");
+            driver.FindElement(By.ClassName("btn-block")).Click();
 
-            //IWebElement element = driver.FindElement(By.Id(buscaNombre));
-            //String textoAtributo = element.GetAttribute("name");
-            //Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(nombreModifico, textoAtributo);
+            //Busca url 
+            driver.Navigate().GoToUrl(url + "/Recetas");
 
-            //driver.Close();
-            //driver.Quit();
+
+            try
+            {
+                IWebElement elementActivo = driver.FindElement(By.Id(buscaNombre));
+                textoAtributo = elementActivo.GetAttribute("checked");
+                if (textoAtributo == "true")
+                {
+                    //Presiono click 
+                    driver.FindElement(By.Id(idBusca)).Click();
+                }
+                //Modifico Campos
+                driver.FindElement(By.Id("estado")).Click();
+                //click boton
+                driver.FindElement(By.ClassName("btn")).Click();
+
+                //Busco elemento id para optener atributos y verificar  que el id existe 
+                IWebElement element = driver.FindElement(By.Id(buscaNombre));
+                textoAtributo = element.GetAttribute("checked");
+            }
+            catch (Exception)
+            {
+                textoAtributo = "No exite id " + idBusca;
+            }
+
+                //comparo elementos
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual("true", textoAtributo);
+
+            // Cierro Driver y ventana 
+            driver.Close();
+            driver.Quit();
         }
+
+        [Test]
+        public void RegistraInsumoTest()
+        {
+            // Variables para busqueda 
+            string idBusca = "1";
+            string buscaTexto = "insumo " + idBusca;
+            string seleccionInsumo = "Sal";
+
+            // Ingreso Login
+            driver.Navigate().GoToUrl(url + "/Home/Login");
+            driver.FindElement(By.Id("nombre")).SendKeys("ariel");
+            driver.FindElement(By.Id("clave")).SendKeys("1234");
+            driver.FindElement(By.ClassName("btn-block")).Click();
+
+            //Busca url 
+            driver.Navigate().GoToUrl(url + "/DetalleReceta/Index/"+ idBusca);
+
+            //atrapo error al no encontrar elemento
+            try { 
+            
+                //Busco elemento tag para optener atributos
+                IWebElement elementValido = driver.FindElement(By.Id(seleccionInsumo));
+
+                //click  boton eliminar
+                driver.FindElement(By.Id(seleccionInsumo + " " + idBusca)).Click();
+            }
+            catch (Exception)
+            {
+
+                //envio a consola la captura
+                Console.WriteLine("No existe producto");
+            }
+            //ejemplo de buscar select en combo 
+            // genero mi elemento web con mi etiqueta select.
+            IWebElement seleccion = driver.FindElement(By.TagName("select"));
+
+            //listo mis elementos, se debe agregar: using System.Collections.Generic;
+            IList<IWebElement> selecciones = seleccion.FindElements(By.TagName("option"));
+
+            //listo y busco el value que quiero seleccionar
+            foreach (IWebElement option in selecciones)
+            {
+                Console.WriteLine(seleccionInsumo + option.GetAttribute("value"));
+                option.Click();
+            }
+
+            //Agrego texto
+            driver.FindElement(By.Id("txtCantidad")).SendKeys("11");
+
+            //click  boton
+            driver.FindElement(By.ClassName("btn")).Click();
+
+            //verificar  que el insumo existe 
+            IWebElement element = driver.FindElement(By.Id(seleccionInsumo));
+            String textoAtributo = element.GetAttribute("Id");
+
+            //comparo elementos
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(seleccionInsumo, textoAtributo);
+
+            //Cierro Driver y ventana 
+            driver.Close();
+            driver.Quit();
+        }
+
+        [Test]
+        public void EliminaInsumoTest()
+        {
+            // Variables para busqueda 
+            string idBusca = "1";
+            string buscaTexto = "insumo " + idBusca;
+            string seleccionInsumo = "Sal";
+            String textoAtributo = "Nulo";
+
+            // Ingreso Login
+            driver.Navigate().GoToUrl(url + "/Home/Login");
+            driver.FindElement(By.Id("nombre")).SendKeys("ariel");
+            driver.FindElement(By.Id("clave")).SendKeys("1234");
+            driver.FindElement(By.ClassName("btn-block")).Click();
+
+            //Busca url 
+            driver.Navigate().GoToUrl(url + "/DetalleReceta/Index/" + idBusca);
+
+            //atrapo error al no encontrar elemento
+            try
+            {
+
+                //Busco elemento tag para optener atributos
+                IWebElement elementValido = driver.FindElement(By.Id(seleccionInsumo));
+            }
+            catch (Exception)
+            {
+
+                //ejemplo de buscar select en combo 
+                // genero mi elemento web con mi etiqueta select.
+                IWebElement seleccion = driver.FindElement(By.TagName("select"));
+
+                //listo mis elementos, se debe agregar: using System.Collections.Generic;
+                IList<IWebElement> selecciones = seleccion.FindElements(By.TagName("option"));
+
+                //listo y busco el value que quiero seleccionar
+                foreach (IWebElement option in selecciones)
+                {
+                    Console.WriteLine(seleccionInsumo + option.GetAttribute("value"));
+                    option.Click();
+                }
+
+                //Agrego texto
+                driver.FindElement(By.Id("txtCantidad")).SendKeys("11");
+
+                //click  boton
+                driver.FindElement(By.ClassName("btn")).Click();
+                //Busco elemento tag para optener atributos
+                IWebElement elementElimino = driver.FindElement(By.Id(seleccionInsumo));
+            }
+
+            //click  boton eliminar
+            driver.FindElement(By.Id(seleccionInsumo + " " + idBusca)).Click();
+
+            //verificar  que el insumo existe 
+            try
+            {
+                
+                IWebElement element = driver.FindElement(By.Id(seleccionInsumo));
+                 textoAtributo = element.GetAttribute("Id");
+            }
+            catch (Exception)
+            {
+                //asigno valor para comparar
+                textoAtributo = "";
+            }
+                //comparo elementos
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(seleccionInsumo, textoAtributo);
+
+                //Cierro Driver y ventana 
+                driver.Close();
+                driver.Quit();
+            }
     }
 }
